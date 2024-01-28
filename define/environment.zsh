76b412de3e0199d962f12cd9102a8b3f3fe5a1a3
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-01-27 21:36:41 -0500

    Commit

--- define/environment.zsh
+++ define/environment.zsh
@@ -4,9 +4,12 @@ EDITOR='/opt/homebrew/bin/nano' VISUAL=$EDITOR
 BAT_PAGER='/bin/sh -c "/usr/bin/less \"$LESS\""'
 
 TMPDIR='/private/var/folders/5p/gq6f59c92z7069cb22k_sj100000gn/T'
-HB_CNF_HANDLER="/opt/homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
 GPG_TTY=$TTY
 
 #if [[ $LESS != (* |)-([^ ]##|)r([^ ]##|)( *|) ]]
 LESS="$LESS -r"
+#endif
+
+#if [[ -z $HB_CNF_HANDLER ]]
+HB_CNF_HANDLER="/opt/homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
 #endif
