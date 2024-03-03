c79ceac9d79750af739162849c2d86ae9afd6b34
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-03 18:18:09 -0500

    Changes

--- define/variables.zsh
+++ define/variables.zsh
@@ -16,16 +16,6 @@ path=( $zpath $path )
 
 fpath=( $ZDOTDIR/functions $ZDOTDIR/functions/**(-/N) $HOMEBREW_PREFIX/opt/curl/share/zsh/site-functions $HOMEBREW_PREFIX/share/zsh-completions $HOMEBREW_PREFIX/share/zsh/site-functions $HOMEBREW_PREFIX/share/zsh/functions $fpath )
 
-# #if [[ -t 1 ]]
-# #if [[ $TERM_PROGRAM == "vscode" ]]
-# #exec printf '\e]11;?\a'; read -rsd $'\\' < /dev/tty;
-# BG=$REPLY$'\\'
-# #else
-# #exec printf '\e]11;?\a'; read -rsd $'\a' < /dev/tty;
-# BG=$REPLY$'\a'
-# #fi
-# #fi
-
 fpath[${fpath[(i)/usr/local/share/zsh/site-functions]}]=( )
 
 module_path=( $module_path $HOMEBREW_PREFIX/lib/zsh )
