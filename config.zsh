ddca2d15502bdf43644d42c0618d230dc8e936e8
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-11 09:54:07 -0400

    Fix

--- config.zsh
+++ config.zsh
@@ -1,1 +1,8 @@
 SAVEHIST=10000000 HISTSIZE=100000
+SAVEHIST=10000000 HISTSIZE=100000
+export HOMEBREW_PREFIX='/opt/homebrew'
+export HOMEBREW_PREFIX='/opt/homebrew'
+export HOMEBREW_CELLAR='/opt/homebrew/Cellar'
+export HOMEBREW_CELLAR='/opt/homebrew/Cellar'
+export HOMEBREW_REPOSITORY='/opt/homebrew'
+export HOMEBREW_REPOSITORY='/opt/homebrew'
