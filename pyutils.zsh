f43429da421da0c4b678066f061d469ac7e41212
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-11 09:48:55 -0400

    Better install script

--- pyutils.zsh
+++ pyutils.zsh
@@ -1,16 +1,9 @@
-py() {
-  if [[ $0 == py ]] {
-    python3 "$@"
-  } else {
-    ${0/py/python} "$@"
-  }
-}
-
 () {
   local name
   for name ( ${commands[(I)python([0-9]##((.~?(#e))|(#e)))(#c0,2)]} ) {
-    name=py${name:6}
+    pyname=py${name:6}
 
-    functions -c py $name
+    alias $pyname=$name
+    alias py=$name
   }
 }
