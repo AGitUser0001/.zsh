50d08fd4005245297bd11481386c040cee8170ea
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2025-09-24 20:43:18 -0400

    Change elapsed time calculations and add auto-pop functionality to define

--- utils.zsh
+++ utils.zsh
@@ -33,25 +33,50 @@ define() {
     (0) builtin print -u2 "Usage: $funcstack[1] <file> [<command>]"; builtin return 1;;
     (1) 2=$funcstack[1];;
   }
-  local args stack=() command file=$1
+  local args stack=() stack_autopop=()  command file=$1
   builtin shift
   while builtin read -r command || [[ $command ]] {
+    local temp_autopop=0
 
     case $command {
       ('');;
+      (\#@<->([[:space:]]*|))
+      temp_autopop="${${command#\#@}%%[[:space:]]*}"
+      if (( !temp_autopop )) {
+        temp_autopop=-1
+      }
+      command="#${command#\#@*[[:space:]]}"
+      if [[ "$command" == "" ]] {
+        if (( $#stack_autopop && temp_autopop )) {
+          stack_autopop[-1]=$temp_autopop
+        }
+      }; ;|
+      (\#@<->);;
       (\#*)
       local action="${${command#\#}%%[[:space:]]*}"
       local data="${${command#\#}#*[[:space:]]}"
       case $action {
         ('');;
         (args) args="$data";;
-        (if) builtin eval "$data"; stack+=$?;;
-        (elif) if (( stack[-1] )) { builtin eval "$data"; stack[-1]=$? } else { stack[-1]=0 } ;;
-        (else) stack[-1]=$(( !stack[-1] ));; (fi) builtin shift -p stack;;
+        (if) builtin eval "$data"; stack+=$?; stack_autopop+=$temp_autopop;;
+        (elif) stack_autopop[-1]=$temp_autopop;
+               if (( stack[-1] )) { builtin eval "$data"; stack[-1]=$? } else { stack[-1]=0 } ;;
+        (else) stack[-1]=$(( !stack[-1] )); stack_autopop[-1]=$temp_autopop;; (fi) builtin shift -p stack stack_autopop;;
         (exec) if (( 0${(j"")stack} == 0 )) { builtin eval "$data"; }; ;;
         (*) console:error invalid action: $action;;;
-      };;
-      (*) if (( 0${(j"")stack} == 0 )) { builtin eval "${(q)@}" $args $command; }; ;;
+      }; ;;
+      (*)
+      if (( stack_autopop[-1] < 0 )) {
+        builtin shift -p stack stack_autopop
+      }
+      if (( 0${(j"")stack} == 0 )) { builtin eval "${(q)@}" $args $command; };
+      if (( stack_autopop[-1] > 0 )) {
+        (( stack_autopop[-1]-- ))
+        if (( stack_autopop[-1] == 0 )) {
+          builtin shift -p stack stack_autopop
+        }
+      }
+      ;;
     }
   } < $file
 }
